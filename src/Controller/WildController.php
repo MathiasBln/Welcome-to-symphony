929a0997d7abc9fed72cfc89942e27b5f4c9153d
Mathias <mlj.bouillon@outlook.fr>
2019-11-21 15:46:49 +0100

    Symfony quest 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -8,10 +8,31 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WildController extends AbstractController
 {
+    /**
+     * @Route("wild/", name="wild_index")
+     */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/wild/show/{slug}",
+     *      requirements={"slug"="[a-z0-9\-]+"},
+     *      methods={"GET"},
+     *      defaults={"slug" = "Aucune série sélectionnée, veuillez choisir une série"},
+     *      name="wild_show"
+     * )
+     */
+    public function show(string $slug): Response
+    {
+            $slug = str_replace("-", " ", $slug);
+            $slug = ucwords($slug);
+
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
         ]);
     }
 }
